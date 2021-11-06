8773fbebba7dac3cd69a7dd606beb652e21a45fb
Erik Luebbers <erluebbers@gmail.com>
2021-11-06 09:12:05 -0700

    commit tree 0a56e3f511f711097c5e950f57e8acaa3ccc9d85

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solution here
+function findMatching (array, name) {
+    const matchedDriver = array.filter(function (element) {
+        return element.toLowerCase() === name.toLowerCase()
+    })
+    return matchedDriver
+}
+function fuzzyMatch (array, string) {
+    const foundDriver = array.filter( function (element) {
+        return element[0] === string[0] && element[1] === string[1]
+    })
+    return foundDriver
+}
+function matchName (array, string) {
+    const driverList = array.filter(function (element) {
+        return element.name === string
+    })
+    return driverList
+}
